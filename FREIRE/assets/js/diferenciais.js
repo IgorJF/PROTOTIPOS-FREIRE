84a4bf57bfdfb54b4d04c3bfa23fbe872f42c90a
IgorJF <111748228+IgorJF@users.noreply.github.com>
2024-04-22 10:01:06 -0300

    beta att

---
+++ FREIRE/assets/js/diferenciais.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var tratamentoCards = document.querySelectorAll('.diferenciais-card');
+
+    tratamentoCards.forEach(function (card) {
+        var imagem = card.querySelector('img');
+        var infoTratamento = card.querySelector('.info-diferenciais');
+        
+        card.addEventListener('click', function () {
+            var link = card.querySelector('a');
+            if (link) {
+                var href = link.getAttribute('href');
+                if (href) {
+                    window.location.href = href;
+                }
+            }
+        });
+
+        imagem.addEventListener('mouseenter', function() {
+            infoTratamento.style.backgroundColor = 'rgba(46, 46, 46, 0)';
+        });
+        
+        imagem.addEventListener('mouseleave', function() {
+            infoTratamento.style.backgroundColor = 'rgba(46, 46, 46, 0.5)';
+        });
+    });
+});
